7e33231ce3ea841271bd84a22011a73be7f37157
Stephen Grice <stephengrice@live.com>
2016-10-28 15:41:34 -0400

    Add smooth scroll to nav buttons

--- js/main.js
+++ js/main.js
@@ -1,13 +1,36 @@
-var links = $(".links");
-var portfolio = $("#portfolio");
-var wdw = $(window)
+$(document).ready(function() {
+	// Sticky nav links
+	var links = $(".links");
+	var portfolio = $("#portfolio");
+	var wdw = $(window)
 
-wdw.scroll(function(e) {
-    if (wdw.scrollTop() > wdw.height() * .95) {
-        links.addClass("links-state2");
-        portfolio.addClass("portfolio-state2");
-    } else {
-        links.removeClass("links-state2");
-        portfolio.removeClass("portfolio-state2");
-    }
+	wdw.scroll(function(e) {
+		if (wdw.scrollTop() > wdw.height() * .95) {
+			links.addClass("links-state2");
+			portfolio.addClass("portfolio-state2");
+		} else {
+			links.removeClass("links-state2");
+			portfolio.removeClass("portfolio-state2");
+		}
+	});
+
+	// Add scrolling
+	$("#nav_portfolio").click(function() {
+		$('html, body').animate({
+			scrollTop: $("#portfolio").offset().top
+		}, 1000);
+		return false;
+	});
+	$("#nav_top").click(function() {
+		$('html, body').animate({
+			scrollTop: $("#top").offset().top
+		}, 1000);
+		return false;
+	});
+	$("#nav_resume").click(function() {
+		$('html, body').animate({
+			scrollTop: $("#resume").offset().top
+		}, 1000);
+		return false;
+	});
 });
