da37698837dbff3f8f66cd232b1b3c34ad938eef
Stephen Grice <stephengrice@live.com>
2016-10-29 17:13:33 -0400

    Make nav fixed, add Flickity carousels

--- js/main.js
+++ js/main.js
@@ -1,36 +1,28 @@
+var TRANSITION_SPEED = 700;
+
 $(document).ready(function() {
 	// Sticky nav links
 	var links = $(".links");
 	var portfolio = $("#portfolio");
 	var wdw = $(window)
 
-	wdw.scroll(function(e) {
-		if (wdw.scrollTop() > wdw.height() * .95) {
-			links.addClass("links-state2");
-			portfolio.addClass("portfolio-state2");
-		} else {
-			links.removeClass("links-state2");
-			portfolio.removeClass("portfolio-state2");
-		}
-	});
-
 	// Add scrolling
 	$("#nav_portfolio").click(function() {
 		$('html, body').animate({
 			scrollTop: $("#portfolio").offset().top
-		}, 1000);
+		}, TRANSITION_SPEED);
 		return false;
 	});
 	$("#nav_top").click(function() {
 		$('html, body').animate({
 			scrollTop: $("#top").offset().top
-		}, 1000);
+		}, TRANSITION_SPEED);
 		return false;
 	});
 	$("#nav_resume").click(function() {
 		$('html, body').animate({
 			scrollTop: $("#resume").offset().top
-		}, 1000);
+		}, TRANSITION_SPEED);
 		return false;
 	});
 });
