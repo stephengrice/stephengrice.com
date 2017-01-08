5939971fcc66c3f38c7c82555fd8e8b41780f71c
Stephen Grice <stephengrice@live.com>
2017-01-08 14:44:58 -0500

    Flesh out new site design

--- js/main.js
+++ js/main.js
@@ -1,47 +1,17 @@
-var TRANSITION_SPEED = 700;
+window.onload = function() {
+    document.getElementById("footer_year").innerHTML = new Date().getFullYear();
+    setGreeting();
+};
 
-$(document).ready(function() {
-	// Sticky nav links
-	var links = $(".links");
-	var portfolio = $("#portfolio");
-	var wdw = $(window)
-
-	// Add scrolling
-	$("#nav_portfolio").click(function() {
-		$('html, body').animate({
-			scrollTop: $("#portfolio").offset().top
-		}, TRANSITION_SPEED);
-		return false;
-	});
-	$("#nav_top").click(function() {
-		$('html, body').animate({
-			scrollTop: $("#top").offset().top
-		}, TRANSITION_SPEED);
-		return false;
-	});
-	$("#nav_resume").click(function() {
-		$('html, body').animate({
-			scrollTop: $("#resume").offset().top
-		}, TRANSITION_SPEED);
-		return false;
-	});
-	
-	responsiveStyle();
-	// Responsive check
-	wdw.resize(function() {
-		responsiveStyle();
-	});
-});
-
-function responsiveStyle() {
-	console.log("isMobile(): " + isMobile() + " viewport: " + $(window).width());
-	if (isMobile()) {
-		$("#nav_top").insertBefore("#nav_portfolio");
-	} else {
-		$("#nav_portfolio").insertBefore("#nav_top");
-	}
-}
-
-function isMobile() {
-	return $("#mobile").css("visibility") == "visible";
+function setGreeting() {
+    var hour = new Date().getHours();
+    var greeting;
+    if (hour < 12) {
+        greeting = "Good evening.";
+    } else if (hour < 18) {
+        greeting = "Good afternoon.";
+    } else {
+        greeting = "Good evening.";
+    }
+    document.getElementById("main_greeting").innerHTML = greeting;
 }
