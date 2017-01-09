be26260112a5806b4c8ec9a956b1685f288beca8
Stephen Grice <stephengrice@live.com>
2017-01-09 13:02:14 -0500

    Fix main images breakpoint for mobile

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
+var IMAGES_BREAKPOINT = 794;
+
 window.onload = function() {
     document.getElementById("footer_year").innerHTML = new Date().getFullYear() + " ";
-    console.log(location.pathname);
     if (location.pathname == "/" || location.pathname == "/index.html") {
         setGreeting();
     }
+	resizeFix();
 };
+
+window.onresize = resizeFix;
 
 function setGreeting() {
     var hour = new Date().getHours();
@@ -17,4 +21,14 @@ function setGreeting() {
         greeting = "Good evening.";
     }
     document.getElementById("main_greeting").innerHTML = greeting;
+}
+
+function resizeFix() {
+	var github = document.getElementById("social_github");
+	if (window.innerWidth <= 794) {
+		// Move github below the avatar image
+		github.parentElement.insertBefore(document.getElementById("avatar"), github);
+	} else {
+		github.parentElement.insertBefore(github, document.getElementById("avatar"));
+	}
 }
